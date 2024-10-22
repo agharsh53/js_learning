5f7a13c8f87093df72a14f93769503c1384160ae
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-10-22 14:40:59 +0000

    update agharsh53.js

--- DOM_Projects/4-GuessTheNumber/agharsh53.js
+++ DOM_Projects/4-GuessTheNumber/agharsh53.js
@@ -1,4 +1,4 @@
-const randomNumber  = parseInt(Math.random()*100+1);
+let randomNumber  = parseInt(Math.random()*100+1);
 
 const submit = document.querySelector('#subt')
 const userInput = document.querySelector('#guessField')
