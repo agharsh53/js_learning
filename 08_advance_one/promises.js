a1d2c4f4152ae05c750187445090b5da585c4c97
Harsh Kumar Agrawal <agrawalharsh2653@gmail.com>
2024-10-28 08:47:34 +0000

    rejected concept in promises in js

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -32,3 +32,23 @@ promiseThree.then(function(user){
       console.log(`Username: ${user.username}`)
       console.log(`Email Id: ${user.email}`)
 })
+
+const promiseFour = new Promise(function(resolve,rejected){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({username:"Harsh", password:"1234"});
+        }else{
+            rejected('Error: Something went wrong')
+        }
+    },1000)
+})
+
+promiseFour.then((user)=>{
+console.log(user);
+return user.username
+}).then((username)=>{
+    console.log(username)
+}).catch(function(error){
+    console.log(error)
+}).finally(()=> console.log("The promise is either resolve or rejected"))
